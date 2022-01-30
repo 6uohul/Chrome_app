5d9f540b6ea16dcd79d9c23b549fff5c48075c46
6uohul <kiy010604@naver.com>
2022-01-30 23:54:15 +0900

    2022.01.30 events

--- first/app.js
+++ first/app.js
@@ -1,3 +1,17 @@
-const title = document.getElementsByTagName("h1");
+const title = document.querySelector(".hello h1");
 
-console.log(title);
+function handleTitleClick(){
+    title.style.color = "blue";
+}
+
+function handleMouseenter(){
+    title.innerText = "Mouse is here!"
+}
+
+function handleMouseleave(){
+    title.innerText = "Mouse is gone!";
+}
+
+title.addEventListener("click", handleTitleClick);  //괄호 금지!!
+title.addEventListener("mouseenter", handleMouseenter);
+title.addEventListener("mouseleave", handleMouseleave);
