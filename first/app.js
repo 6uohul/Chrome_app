34067884e119cb3c69777fcfe88deb8086bc811b
6uohul <kiy010604@naver.com>
2022-01-31 01:14:07 +0900

    JS_toggle

--- first/app.js
+++ first/app.js
@@ -1,38 +1,6 @@
 const h1 = document.querySelector(".hello h1");
 
 function handleTitleClick(){
-    h1.style.color = "blue";
+    h1.classList.toggle("clicked");
 }
-
-function handleMouseenter(){
-    h1.innerText = "Mouse is here!"
-}
-
-function handleMouseleave(){
-    h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy(){
-    alert("Successfully Copied!");
-}
-
-function handleWindowOffline(){
-    alert("SOS no WIFI");
-}
-
-function handleWindowOffline(){
-    alert("WIFI connected");
-}
-
 h1.addEventListener("click", handleTitleClick);  //괄호 금지!!
-h1.addEventListener("mouseenter", handleMouseenter);
-h1.addEventListener("mouseleave", handleMouseleave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
