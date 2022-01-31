10c4fe52db0b063572b13a0156e23e7cf4ccc43c
6uohul <kiy010604@naver.com>
2022-01-31 18:17:48 +0900

    login 구현하기

--- first/app.js
+++ first/app.js
@@ -1,10 +1,30 @@
-const loginForm = document.getElementById("login-form");
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-function handleLoginClick(){
+const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    console.log(username);
+    localStorage.setItem(USERNAME_KEY, username);
+    //greeting.innerText = "Hello " + username;
+    Greetings(username);
 }
 
-loginButton.addEventListener("click", handleLoginClick);
+function Greetings(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const localStUsername = localStorage.getItem(USERNAME_KEY);
+
+if(localStUsername === null){   //user정보가 없을 때
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}else{
+    Greetings(localStUsername);
+}
