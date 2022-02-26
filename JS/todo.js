fc8235f63cf78cef844e3ceae458d905ed6f0f57
6uohul <kiy010604@naver.com>
2022-02-27 00:42:07 +0900

    localStorage에  성공적으로 저장하기

--- JS/todo.js
+++ JS/todo.js
@@ -2,6 +2,14 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY="todos"
+let toDos = [];
+
+//localStorage에는 string만 저장이 되므로 string으로 바꿈
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
 function deleteToDo(event){
   const li = event.target.parentElement;
   li.remove();
@@ -23,7 +31,17 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value = "";
-    paintToDo(newToDo)
+    toDos.push(newToDo);
+    paintToDo(newToDo);
+    saveToDos();
 }
 
-toDoForm.addEventListener("submit", handleToDoSubmit)
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+if(savedToDos !== null){
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+    //문자열 --> 배열
+}
