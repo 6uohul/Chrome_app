ea801e45e61b3ee90b3188f94ab859a85261eb18
6uohul <kiy010604@naver.com>
2022-02-26 23:54:51 +0900

    delete to-do-list

--- JS/todo.js
+++ JS/todo.js
@@ -2,11 +2,20 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event){
+  const li = event.target.parentElement;
+  li.remove();
+}
+
 function paintToDo(newToDo){
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);   //li는 span이라는 자식을 가짐
+    const button = document.createElement("button");
     span.innerText = newToDo;
+    button.innerText = "❌";
+    button.addEventListener("click", deleteToDo)
+    li.appendChild(span);   //li는 span이라는 자식을 가짐
+    li.appendChild(button);
     toDoList.appendChild(li);
 }
 
