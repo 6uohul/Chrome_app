821b4c20d3efeec4e4e05104f0ace74233d5b43d
6uohul <kiy010604@naver.com>
2022-03-01 01:25:31 +0900

    weather API

--- JS/weather.js
+++ JS/weather.js
@@ -1,6 +1,15 @@
+const weather = document.querySelector("#weather span:first-child");
+ const city = document.querySelector("#weather span:last-child");
+const API_KEY="a33bd6734ac0f19fbc4937876e599fae";
+
 function onGeoOK(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json()).then((data) => {
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      });;
 }
 
 function onGoeErr(){
@@ -8,4 +17,4 @@ function onGoeErr(){
 }
 
 // 첫번째 argument : 성공적으로 실행됐을 때 , 두번째 argument : 실패했을 때
-navigator.geolocation.getCurrentPosition();
+navigator.geolocation.getCurrentPosition(onGeoOK, onGoeErr);
